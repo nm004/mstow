cf1ae22ead42d69809cd6c7c5cd9d4890cd9b3b4
Nozomi Miyamori <inspc43313@yahoo.co.jp>
2022-09-29 21:37:51 +0900

    refactor code

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,4 @@
-/*
- * Mstow - Miyamori's minimal GNU Stow implementation
- *
- * Copyright (c) 2022 Nozomi Miyamori
- *
- * Permission is hereby granted, free of charge, to any person obtaining a copy of this
- * software and associated documentation files (the "Software"), to deal in the Software
- * without restriction, including without limitation the rights to use, copy, modify,
- * merge, publish, distribute, sublicense, and/or sell copies of the Software, and to
- * permit persons to whom the Software is furnished to do so.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,
- * INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A
- * PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
- * HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
- * OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
- * SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
- */
+// This program is in the public domain.
 
 use clap::Parser;
 use clap_verbosity_flag::{Verbosity, WarnLevel};
@@ -90,7 +73,7 @@ fn main() {
         ok_or_abort!(ll);
 
         let ll = ll.unwrap();
-        let mut ll = ll.iter().flatten();
+        let ll = ll.iter().flatten();
 
         let maybe_not_conflicted = ll.clone().try_fold(HashMap::new(), |mut acc, i| {
             let r = acc.insert(i.0.clone(), i.1.clone());
